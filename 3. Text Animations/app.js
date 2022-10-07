9ad84926ab06db22e1a6bd555160fb7df2d111dc
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2022-10-07 17:24:40 +0600

    Micro animation starter added

--- 3. Text Animations/app.js
+++ 3. Text Animations/app.js
@@ -23,3 +23,17 @@ tl.fromTo('.cta6', { x: '-100%', opacity: 0 }, { x: 0, opacity: 1 }, "<20%");
 tl.fromTo('.cta5', { y: '100%', opacity: 0 }, { y: 0, opacity: 1 }, "<20%");
 
 tl.fromTo('.cta-btn', { y: 20, opacity: 0 }, { y: 0, opacity: 1 }, "<");
+
+// Split Text alternative
+const logo = document.querySelector('.logo');
+const letters = logo.textContent.split('');
+
+logo.textContent = "";
+
+letters.forEach((letter) => {
+    logo.innerHTML += `<span class="letter">${letter}</span>`;
+});
+// We need to set the display of each letter to "inline-block" as otherwise the animations won't work on the spans
+gsap.set('.letter', { display: 'inline-block' });
+// Stagger makes the animations trigger one after another after with delays set depending on the value we pass into it
+gsap.fromTo('.letter', { y: '100%' }, { y: 0, delay: 2, stagger: 0.05, ease: Back.easeOut.config(3) });
